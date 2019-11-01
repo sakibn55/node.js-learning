212e58fa50e3de4553236dda88151739ad28158e
Nazmus Sakib <sakibn55@gmail.com>
2019-11-01 02:24:22 -0400

    added views

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.use("/", (req, res, next) => {
-  res.send("<h1>Home</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
