565ab184832ef12a7ed0baba9ad0fa38a5380557
Nazmus Sakib <sakibn55@gmail.com>
2019-11-01 01:25:07 -0400

    added express js and body-parse, simple post request

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const server = http.createServer(routes.handler);
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method= "POST"> <input type="text" name="title"> <button type="submit"> add </button> </form>'
+  );
+});
 
-server.listen(3000);
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Home</h1>");
+});
+
+app.listen(3000);
