561d433bb0bed9d4c11df5223283f21f3714fab1
Nazmus Sakib <sakibn55@gmail.com>
2019-11-01 02:36:44 -0400

    helper for root dir

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,11 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
 const router = express.Router();
 
-router.use("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
