94d67538bcfe8ada0683038cb808897177d3f9a8
Nazmus Sakib <sakibn55@gmail.com>
2019-11-04 00:37:25 -0500

    styled the pages

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
